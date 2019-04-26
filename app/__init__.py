2deccea2061e0b923e80a384131c57ce535284f9
Zidane Wright <zidane.wright@mymona.uwi.edu>
2019-04-26 05:37:35 +0000

    create database models & do initial migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 csrf = CSRFProtect(app)
 app.config['UPLOAD_FOLDER'] = './app/static/uploads'
 app.config['SECRET_KEY'] = 'v\xf9\xf7\x11\x13\x18\xfaMYp\xed_\xe8\xc9w\x06\x8e\xf0f\xd2\xba\xfd\x8c\xda'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:password123@localhost/photogram'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-from app import views
+db = SQLAlchemy(app)
+
+from app import views, models
---
+++ app/models.py
@@ -0,0 +1,62 @@
+import datetime
+from . import db
+
+class Posts(db.Model):
+    __tablename__ = 'posts'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    photo = db.Column(db.String(255))
+    caption = db.Column(db.String(255))
+    created_on = db.Column(db.Date, default=datetime.datetime.now())
+    
+    def __init__(self, user_id, photo, caption):
+        self.user_id = user_id
+        self.photo = photo
+        self.caption = caption
+        
+class Users(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(255))
+    firstname = db.Column(db.String(255))
+    lastname = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True)
+    location = db.Column(db.String(255))
+    biography = db.Column(db.String(255))
+    profile_photo = db.Column(db.String(255))
+    joined_on = db.Column(db.Date, default=datetime.datetime.now())
+    
+    def __init__(self, username, password, firstname, lastname, email, 
+                location, biography, profile_photo):
+                    
+                self.username = username
+                self.password = password
+                self.firstname = firstname
+                self.lastname = lastname
+                self.email = email
+                self.location = location
+                self.biography = biography
+                self.profile_photo = profile_photo
+    
+class Likes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    post_id = db.Column(db.Integer)
+    
+    def __init__(self, user_id, post_id):
+        self.user_id = user_id
+        self.post_id = post_id
+        
+
+class Follows(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    follower_id = db.Column(db.Integer)
+    
+    def __init__(self, user_id, follower_id):
+        self.user_id = user_id
+        self.follower_id = follower_id
+    
