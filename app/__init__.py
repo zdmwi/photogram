3892917e3fd63b06fc241c4001075d4e7382f074
Zidane Wright <zidane.wright@mymona.uwi.edu>
2019-04-28 03:15:24 +0000

    update database uri

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 csrf = CSRFProtect(app)
 app.config['UPLOAD_FOLDER'] = './app/static/uploads'
 app.config['SECRET_KEY'] = 'v\xf9\xf7\x11\x13\x18\xfaMYp\xed_\xe8\xc9w\x06\x8e\xf0f\xd2\xba\xfd\x8c\xda'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:password123@localhost/photogram'
+# DATABASE_URL = 'postgresql://admin:password123@localhost/photogram'
+DATABASE_URL='postgresql://xcdatgokkmkige:a2030919439e0888d0c95a716a437623b2ae94a97f340aca4e579efdfbf15df0@ec2-54-225-129-101.compute-1.amazonaws.com:5432/d6gj96ea29eaei'
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
